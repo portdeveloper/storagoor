125709d37f1cb23c6713f103ab0415ebce11053a
port <schoolreef2@protonmail.com>
2023-04-01 21:32:58 +0300

    Fix table overflow on mobile

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -164,26 +164,28 @@ const App = () => {
           {hexValue !== "" && (
             <div>
               <h2 className="text-xl sm:text-2xl font-bold mb-4">Result:</h2>
-              <table className="table w-full mx-auto">
-                <tbody>
-                  <tr>
-                    <td className="bg-gray-300 font-mono text-lg text-black">Hexadecimal value:</td>
-                    <td className="bg-gray-700 font-mono text-lg">{hexValue}</td>
-                  </tr>
-                  <tr>
-                    <td className="bg-gray-300 font-mono text-lg text-black">Decimal value:</td>
-                    <td className="bg-gray-700 font-mono text-lg">{numberValue}</td>
-                  </tr>
-                  <tr>
-                    <td className="bg-gray-300 font-mono text-lg text-black">String value:</td>
-                    <td className="bg-gray-700 font-mono text-lg">{stringValue}</td>
-                  </tr>
-                  <tr>
-                    <td className="bg-gray-300 font-mono text-lg text-black">Address value:</td>
-                    <td className="bg-gray-700 font-mono text-lg">{addressValue}</td>
-                  </tr>
-                </tbody>
-              </table>
+              <div className="overflow-x-scroll">
+                <table className="table w-full mx-auto">
+                  <tbody>
+                    <tr>
+                      <td className="bg-gray-300 font-mono text-lg text-black">Hexadecimal value:</td>
+                      <td className="bg-gray-700 font-mono text-lg">{hexValue}</td>
+                    </tr>
+                    <tr>
+                      <td className="bg-gray-300 font-mono text-lg text-black">Decimal value:</td>
+                      <td className="bg-gray-700 font-mono text-lg">{numberValue}</td>
+                    </tr>
+                    <tr>
+                      <td className="bg-gray-300 font-mono text-lg text-black">String value:</td>
+                      <td className="bg-gray-700 font-mono text-lg">{stringValue}</td>
+                    </tr>
+                    <tr>
+                      <td className="bg-gray-300 font-mono text-lg text-black">Address value:</td>
+                      <td className="bg-gray-700 font-mono text-lg">{addressValue}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           )}
         </div>
